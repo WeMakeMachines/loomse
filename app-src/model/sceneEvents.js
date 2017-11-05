cf25968f96bc2632b45e7b5ffa7fa9d57a7c4b23
Franco Speziali <franco@wemakemachines.com>
2017-11-05 15:09:23 +0100

    - Moved Event class into view component - Moved subtitle scss into own file -

--- app-src/model/sceneEvents.js
+++ app-src/model/sceneEvents.js
@@ -1,7 +1,7 @@
 /**
  * Handles all the logic for the scene events
  */
-import data from '../model/data';
+import Event from '../view/components/event';
 import media from '../view/media';
 import sceneEventsView from '../view/sceneEvents';
 import storyBehaviour from '../configs/storyBehaviour';
@@ -33,106 +33,6 @@ let events = {
 		}
 	},
 	previousMediaTimeIndex = 0;
-
-/**
- * Event class
- */
-class Event {
-
-	/**
-	 * @param {string} id
-	 * @param {string} extension
-	 * @param {object} schedule
-	 * @param {object} parameters
-	 * @param {object} element
-	 */
-	constructor(id, extension, schedule, parameters, element) {
-		this.id = id; // event id
-		this.extension = extension;
-		this.state = 'waiting'; // waiting, fired, expired
-		this.in = schedule.in;
-		this.out = schedule.out;
-		this.parameters = parameters;
-		this.class = parameters.class;
-		this.element = element;
-	}
-
-	/**
-	 * Checks if the event is ready to fire
-	 * @param {number} time
-	 * @returns {boolean}
-	 */
-	checkShouldFire(time) {
-		return time >= this.in && this.state === 'waiting';
-	}
-
-	/**
-	 * Checks if the event should expire
-	 * @param {number} time
-	 * @returns {boolean}
-	 */
-	checkShouldExpire(time) {
-		return time >= this.out && this.state === 'fired';
-	}
-
-	/**
-	 * Checks if the event should perform an action
-	 * @param {number} time
-	 * @returns {boolean}
-	 */
-	checkSchedule(time) {
-		if (this.state === 'expired') {
-			return false;
-		}
-
-		if (this.checkShouldFire(time)) {
-			this.run();
-			this.state = 'fired';
-		}
-
-		if (this.checkShouldExpire(time)) {
-			this.kill();
-			this.state = 'expired';
-		}
-	}
-
-	/**
-	 * Fixes the current state after time index has jumped, say after seeking
-	 * @param {number} time
-	 */
-	fixState(time) {
-		if (time >= this.out || this.state === 'fired') {
-			this.state = 'expired';
-		}
-
-		if (time <= this.in) {
-			this.state = 'waiting';
-		}
-	}
-
-	/**
-	 * Runs the event
-	 */
-	run() {
-		this.extension.run.call(this, this.element.node, () => {
-			this.render();
-		});
-	}
-
-	/**
-	 * Stops the event and removes any performed actions
-	 */
-	kill() {
-		this.extension.stop();
-	}
-
-	/**
-	 * Renders the event into the DOM
-	 */
-	render() {
-		this.element.setPosition(data.dimensions, this.parameters.x, this.parameters.y);
-	}
-}
 
 /**
  * Checks for a valid API of the event  extension
---
+++ app-src/view/components/event.js
@@ -0,0 +1,102 @@
+import data from '../../model/data';
+
+/**
+ * Event class
+ */
+class Event {
+
+	/**
+	 * @param {string} id
+	 * @param {string} extension
+	 * @param {object} schedule
+	 * @param {object} parameters
+	 * @param {object} element
+	 */
+	constructor(id, extension, schedule, parameters, element) {
+		this.id = id; // event id
+		this.extension = extension;
+		this.state = 'waiting'; // waiting, fired, expired
+		this.in = schedule.in;
+		this.out = schedule.out;
+		this.parameters = parameters;
+		this.class = parameters.class;
+		this.element = element;
+	}
+
+	/**
+	 * Checks if the event is ready to fire
+	 * @param {number} time
+	 * @returns {boolean}
+	 */
+	checkShouldFire(time) {
+		return time >= this.in && this.state === 'waiting';
+	}
+
+	/**
+	 * Checks if the event should expire
+	 * @param {number} time
+	 * @returns {boolean}
+	 */
+	checkShouldExpire(time) {
+		return time >= this.out && this.state === 'fired';
+	}
+
+	/**
+	 * Checks if the event should perform an action
+	 * @param {number} time
+	 */
+	checkSchedule(time) {
+		if (this.state === 'expired') {
+			return;
+		}
+
+		if (this.checkShouldFire(time)) {
+			this.run();
+			this.state = 'fired';
+		}
+
+		if (this.checkShouldExpire(time)) {
+			this.kill();
+			this.state = 'expired';
+		}
+	}
+
+	/**
+	 * Fixes the current state after time index has jumped, say after seeking
+	 * @param {number} time
+	 */
+	fixState(time) {
+		if (time >= this.out || this.state === 'fired') {
+			this.state = 'expired';
+		}
+
+		if (time <= this.in) {
+			this.state = 'waiting';
+		}
+	}
+
+	/**
+	 * Runs the event
+	 */
+	run() {
+		this.extension.run.call(this, this.element.node, () => {
+			this.render();
+		});
+	}
+
+	/**
+	 * Stops the event and removes any performed actions
+	 */
+	kill() {
+		this.extension.stop();
+	}
+
+	/**
+	 * Renders the event into the DOM
+	 */
+	render() {
+		this.element.setPosition(data.dimensions, this.parameters.x, this.parameters.y);
+	}
+}
+
+export { Event as default };
