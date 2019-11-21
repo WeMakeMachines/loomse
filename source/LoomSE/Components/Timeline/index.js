8ccff82d3076927ddaf5750b24faa5ba50c11c7f
Franco Speziali <franco@wemakemachines.com>
2019-11-21 21:26:32 +0100

    Added first draft of Timeline component

---
+++ source/LoomSE/Components/Timeline/index.js
@@ -0,0 +1,40 @@
+import { el } from 'redom';
+
+import ProgressCounter from './ProgressCounter';
+import styles from './styles';
+
+import {
+	VIDEO_DURATION_CHANGED,
+	VIDEO_TIMEUPDATE
+} from '../../constants/videoEvents';
+import { radioService } from '../../services';
+
+class Timeline {
+	constructor() {
+		this.el = el(
+			'#loomSE_timeline',
+			{ style: { ...styles } },
+			(this.progressCounter = new ProgressCounter())
+		);
+		this.duration = 999;
+
+		this.durationChangedToken = radioService.register(
+			VIDEO_DURATION_CHANGED,
+			event => {
+				this.duration = event.duration;
+			}
+		);
+
+		this.timeUpdateToken = radioService.register(
+			VIDEO_TIMEUPDATE,
+			this.updateProgress,
+			this
+		);
+	}
+
+	updateProgress(event) {
+		this.progressCounter.update(event.time, this.duration);
+	}
+}
+
+export default Timeline;
---
+++ source/LoomSE/Components/Timeline/styles.js
@@ -0,0 +1,13 @@
+export default {
+	position: 'absolute',
+	bottom: '20px',
+	height: '5px',
+	width: '100%',
+	'background-color': '#fff'
+};
+
+export const progressCounterStyle = {
+	height: '5px',
+	width: '0px',
+	'background-color': 'yellow'
+};
