3e601d4e629d474e492e99305f00da3bf05fdff1
Franco Speziali <franco@wemakemachines.com>
2019-11-21 18:58:48 +0100

    Rewrote Events component

--- source/LoomSE/Components/Events/index.js
+++ source/LoomSE/Components/Events/index.js
@@ -1,1 +1,21 @@
-export { Events as default } from './Events';
+import { eventService } from '../../services';
+
+class Events {
+	constructor(events) {
+		this.events = eventService({
+			events,
+			startEventCallback: this.start,
+			stopEventCallback: this.stop
+		});
+	}
+
+	start(message) {
+		console.log('on');
+	}
+
+	stop(message) {
+		console.log('off');
+	}
+}
+
+export default Events;
