f8000cffe9a98a85d13cdc5e9d8155ae42acb283
Franco Speziali <franco@wemakemachines.com>
2023-07-16 16:41:59 +0200

    Updated rollup configuration to read "main" and "module" properties from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,35 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import { readFileSync } from 'fs';
 
+const pkg = JSON.parse(readFileSync('package.json', { encoding: 'utf8' }));
 const devMode = process.env.mode === 'dev';
 const minify = devMode ? undefined : terser();
 
 export default [
-	// es module output
-	{
-		input: 'source/index.ts',
-		output: {
-			file: 'dist/loomse.es.min.js',
-			format: 'es',
-			exports: 'default',
-			sourcemap: devMode
-		},
-		external: ['redom', 'simple-subtitle-parser'],
-		plugins: [typescript(), minify]
-	},
 	// umd output
 	{
 		input: 'source/index.ts',
 		output: {
-			file: 'dist/loomse.min.js',
+			file: pkg.main,
 			format: 'umd',
-			name: 'LoomSE',
+			exports: 'named',
+			name: 'loomse',
 			sourcemap: devMode
 		},
 		plugins: [typescript(), nodeResolve(), minify]
+	},
+	// es module output
+	{
+		input: 'source/index.ts',
+		output: {
+			file: pkg.module,
+			format: 'es',
+			exports: 'named',
+			sourcemap: devMode
+		},
+		external: ['redom', 'simple-subtitle-parser'],
+		plugins: [typescript(), minify]
 	}
 ];
