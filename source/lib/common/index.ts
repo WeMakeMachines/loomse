608b78dc8cc33a1cbba0c5a98252dea1b0c89ece
Franco Speziali <franco@wemakemachines.com>
2023-07-16 16:32:09 +0200

    Removed unused library functions

--- source/lib/common/index.ts
+++ source/lib/common/index.ts
@@ -1,29 +1,4 @@
 /**
- * Common tools
- */
-
-export function cleanString(string: string) {
-	return string.replace(/\s+/g, '').toLowerCase();
-}
-
-/**
- * Prevents a function from being called repeatedly
- */
-export const debounce = (() => {
-	let delayedFunction: ReturnType<typeof setTimeout>;
-
-	return (callback: () => void, delay: number) => {
-		if (delayedFunction) {
-			clearTimeout(delayedFunction);
-		}
-
-		delayedFunction = setTimeout(() => {
-			callback();
-		}, delay);
-	};
-})();
-
-/**
  * Checks if an object is empty
  */
 export function isEmptyObject(object: { [key: string]: any }) {
