e5ae33ecfecd50a94faa965b3016bf4dca415c5d
Franco Speziali <franco@wemakemachines.com>
2023-07-19 22:16:19 +0200

    Replaced rollup with tsup

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+	{
+		format: 'esm',
+		entry: { 'loomse.esm': 'source/index.ts' },
+		outDir: './dist',
+		dts: 'source/index.ts',
+		splitting: false,
+		sourcemap: true,
+		clean: true
+	},
+	{
+		format: 'esm',
+		entry: { 'loomse.cjs': 'source/index.ts' },
+		outDir: './dist',
+		dts: 'source/index.ts',
+		splitting: false,
+		sourcemap: true,
+		clean: true
+	}
+]);
