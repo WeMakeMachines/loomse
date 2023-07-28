4bfbca185d06ee7be99ad7dd2b8bfad515b85498
Franco Speziali <franco@wemakemachines.com>
2023-07-28 22:42:58 +0200

    Added build:dev to scripts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,23 +1,25 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig([
-	{
-		format: 'esm',
-		entry: { 'loomse.esm': 'source/index.ts' },
-		outDir: './dist',
-		dts: 'source/index.ts',
-		splitting: false,
-		sourcemap: true,
-		clean: true,
-		publicDir: true
-	},
-	{
-		format: 'esm',
-		entry: { 'loomse.cjs': 'source/index.ts' },
-		outDir: './dist',
-		dts: 'source/index.ts',
-		splitting: false,
-		sourcemap: true,
-		clean: true
-	}
-]);
+export default () => {
+	return defineConfig([
+		{
+			format: 'esm',
+			entry: { 'loomse.esm': 'source/index.ts' },
+			outDir: './dist',
+			dts: 'source/index.ts',
+			splitting: false,
+			clean: true,
+			publicDir: true,
+			sourcemap: !!process.env.NODE_ENV
+		},
+		{
+			format: 'esm',
+			entry: { 'loomse.cjs': 'source/index.ts' },
+			outDir: './dist',
+			dts: 'source/index.ts',
+			splitting: false,
+			clean: true,
+			sourcemap: !!process.env.NODE_ENV
+		}
+	]);
+};
